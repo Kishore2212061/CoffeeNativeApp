bd1daf52fd9964e0c3884c4343c9332bd6023c9c
Kishore2212061 <mersalkishore79@gmail.com>
2025-02-10 15:42:16 +0530

    first Commit

--- app/screens/productDetail/components/customHeader/index.tsx
+++ app/screens/productDetail/components/customHeader/index.tsx
@@ -3,13 +3,11 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { AntDesign, Feather } from "@expo/vector-icons";
-import {styles} from "./style";
+import { styles } from "./style";
 export const CustomHeader = () => {
   const navigation = useNavigation();
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <TouchableOpacity onPress={() => navigation.goBack()}>
         <AntDesign name="arrowleft" size={24} color="black" />
       </TouchableOpacity>
